083b9e855b6668cd8a3bd4e5b85c9a0e229a236c
Chris Mayfield <mayfiecs@jmu.edu>
2016-07-16 10:57:12 -0400

    build all files

--- build.py
+++ build.py
@@ -1,47 +1,61 @@
 """Recursively builds PDF versions of each LaTeX file."""
 
 import os
+import shutil
 import sys
 
+# command to use for os.system
+LATEX = "pdflatex -interaction=nonstopmode"
+
+# remove temporary output files
+CLEAN = True
+
 def build(path, name):
+    """Build the given source file."""
+    if name == "_TEMP_.tex":
+        return
     print path, name,
     cwd = os.getcwd()
     os.chdir(path)
     # create temp activity
-    temp = open("temp.tex", "w")
-    temp.write("\\documentclass[11pt]{article}\n")
-    temp.write("\\usepackage{../../cspogil}\n")
-    temp.write("\\begin{document}\n")
-    temp.write("\\input{" + name + "}\n")
-    temp.write("\\end{document}\n")
-    temp.close()
+    if path.startswith("Models/"):
+        temp = open("_TEMP_.tex", 'w')
+        temp.write("\\documentclass[11pt]{article}\n")
+        temp.write("\\usepackage{../../cspogil}\n")
+        temp.write("\\begin{document}\n")
+        temp.write("\\input{" + name + "}\n")
+        temp.write("\\end{document}\n")
+        temp.close()
+    else:
+        shutil.copyfile(name, "_TEMP_.tex")
     # build and rename pdf
-    os.system("pdflatex -interaction=nonstopmode temp.tex > temp.csm")
-    status = os.system("pdflatex -interaction=nonstopmode temp.tex > temp.csm")
+    os.system(LATEX + " _TEMP_.tex > _TEMP_.run")
+    status = os.system(LATEX + " _TEMP_.tex > _TEMP_.run")
     if status > 0:
         print "ERROR"
     else:
         print "OK"
-        os.rename("temp.pdf", name[:-3] + "pdf")
+        os.rename("_TEMP_.pdf", name[:-3] + "pdf")
     # clean up temp files
-    os.remove("temp.aux")
-    os.remove("temp.csm")
-    os.remove("temp.log")
-    os.remove("temp.out")
-    os.remove("temp.tex")
+    if CLEAN:
+        os.remove("_TEMP_.aux")
+        os.remove("_TEMP_.log")
+        os.remove("_TEMP_.out")
+        os.remove("_TEMP_.run")
+        os.remove("_TEMP_.tex")
     os.chdir(cwd)
 
 def main(args):
-    # build all models
-    for path, dirs, files in os.walk("Models"):
-        for name in files:
-            if name.endswith(".tex"):
-                # TODO check both file modification times
-                # TODO build teacher and student versions
-                build(path, name)
-    # TODO build activities
-    pass
+    """Find and build all files."""
+    for root in ["Activities", "Models"]:
+        for path, dirs, files in os.walk(root):
+            for name in files:
+                if name.endswith(".tex"):
+                    build(path, name)
 
-if __name__ == '__main__':
-    # TODO support for all, clean, student, teacher
+if __name__ == "__main__":
     sys.exit(main(sys.argv))
+
+# TODO build dict of file modification times
+# TODO pdflatex teacher and student versions
+# TODO sysargs: all, clean, student, teacher

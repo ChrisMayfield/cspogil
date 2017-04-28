02fb919e73d6a75bb342d969a507baf1a2e45130
Chris Mayfield <mayfiecs@jmu.edu>
2017-04-28 16:44:16 -0400

    instructor guide for Act06

---
+++ CS1/Act06/Fact.java
@@ -0,0 +1,19 @@
+public class Fact {
+    
+    public static int factorial(int n) {
+        System.out.println("n is " + n);
+        if (n == 0) {
+            return 1;  // base case
+        } else {
+            System.out.printf("need factorial of %d\n", n - 1);
+            int answer = factorial(n - 1);
+            System.out.printf("factorial of %d is %d\n", n - 1, answer);
+            return n * answer;
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(factorial(3));
+    }
+    
+}

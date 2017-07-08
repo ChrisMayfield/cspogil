c03eb45452a191cdd1fbc93d80decdb21ba0e8fb
Chris Mayfield <mayfiecs@jmu.edu>
2017-07-08 07:30:48 -0400

    cs1book builds teacher/student

--- CS1/cs1book.py
+++ CS1/cs1book.py
@@ -10,16 +10,18 @@ def main(suff):
         title = None
         for line in open(name):
             if line.startswith("\\title"):
-                title = line[6:-1]
+                title = line[7:-2]
                 break
-        # output LaTeX
-        print ""
-        print "\\cleardoublepage\\phantomsection"
-        print "\\addcontentsline{toc}{section}" + title
-        print "\\includepdf[pages=-, pagecommand={}]{" + name[:-4] + "_" + suff + ".pdf}"
+        # output results
+        if suff == "tex":
+            print "\\act{" + title + "}{" + name[:-4] + "}"
+        else:
+            title = title[title.find(":")+2:]
+            name = name[:name.find("/")]
+            print "[" + title + "](" + name + ")"
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         main(sys.argv[1])
     else:
-        print("Usage: cs1book.py {Teacher | Student}")
+        print("Usage: cs1book.py {tex | md}")

80a6f575f622beab735cc9440e156044c306a19c
Chris Mayfield <mayfiecs@jmu.edu>
2017-07-10 23:06:36 -0400

    finished Act12 review

--- CS1/Act12/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    
-    public static void main(String[] args) { 
-        Die[] yahtzee = new Die[5];
-        yahtzee[1] = new Die(123, 56);
-        System.out.println(yahtzee[1]);
-    }
-    
-}

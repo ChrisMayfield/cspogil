49850e8963ca7eb961cc516aaf3c029736c18e78
Chris Mayfield <mayfiecs@jmu.edu>
2017-08-25 10:30:56 -0400

    added role-cards-split.sh

---
+++ Handouts/role-cards-split.sh
@@ -0,0 +1,4 @@
+pdftk role-cards-mayfield.pdf cat 1-2 output Manager.pdf
+pdftk role-cards-mayfield.pdf cat 3-4 output Recorder.pdf
+pdftk role-cards-mayfield.pdf cat 5-6 output Presenter.pdf
+pdftk role-cards-mayfield.pdf cat 7-8 output Reflector.pdf

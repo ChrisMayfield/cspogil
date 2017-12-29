de691041c6b1070347ff8351cebdde959ff31748
Chris Mayfield <mayfiecs@jmu.edu>
2017-12-29 11:17:03 -0500

    removed date/149 from drjava

---
+++ CS1/Act01/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * Hello world program.
+ * 
+ * @author Chris Mayfield
+ * @version 1.0
+ */
+public class Hello {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+}

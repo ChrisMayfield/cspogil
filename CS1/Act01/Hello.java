4ff7774d39e0e2d17bc481d3d752c14661ab58ed
Chris Mayfield <mayfiecs@jmu.edu>
2021-07-02 19:47:23 -0400

    revised Act01

--- CS1/Act01/Hello.java
+++ CS1/Act01/Hello.java
@@ -1,13 +1,18 @@
 /**
- * Hello world program.
- * 
+ * The infamous hello world program.
+ *
  * @author Chris Mayfield
  * @version 1.0
  */
-public class Hello {
-    
+public class HelloWorld {
+
+    /**
+     * Prints a hello message to the screen.
+     *
+     * @param args command-line arguments
+     */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-    
+
 }

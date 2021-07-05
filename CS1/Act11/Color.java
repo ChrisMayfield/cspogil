ef78ecc37b463d84c154c1b7ee5bba08d3656543
Chris Mayfield <mayfiecs@jmu.edu>
2021-07-05 16:03:09 -0400

    revised Act11 a lot

--- CS1/Act11/Color.java
+++ CS1/Act11/Color.java
@@ -3,45 +3,44 @@
  * and blue. Each RGB value ranges from 0 to 255 (darkest to lightest).
  */
 public class Color {
-    
+
     private int red;
-    
     private int green;
-    
     private int blue;
-    
+
     /**
-     * Creates a Color object with the specified RGB values.
+     * Constructs the default Color of black.
+     */
+    public Color() {
+        // attributes are automatically initialized to zero
+    }
+
+    /**
+     * Constructs a Color object with the specified RGB values. Any values that
+     * are less than zero are set to zero, and any values that are greater than
+     * 255 are set to 255.
      */
     public Color(int red, int green, int blue) {
-        this.red = check(red);
-        this.green = check(green);
-        this.blue = check(blue);
+        this.red = clip(red);
+        this.green = clip(green);
+        this.blue = clip(blue);
     }
-    
+
     /**
-     * Creates a copy of the given color.
-     * (Not that useful since Color objects are immutable.)
+     * Ensures that the given value is in the range 0 to 255.
      */
-    public Color(Color other) {
-        this.red = other.red;
-        this.green = other.green;
-        this.blue = other.blue;
-    }
-    
-    /**
-     * Ensures the given value is in the range 0 to 255.
-     */
-    private static int check(int value) {
+    private static int clip(int value) {
+        int result;
         if (value < 0) {
-            value = 0;
+            result = 0;
+        } else if (value > 255) {
+            result = 255;
+        } else {
+            result = value;
         }
-        if (value > 255) {
-            value = 255;
-        }
-        return value;
+        return result;
     }
-    
+
     /**
      * Adds the other color to this color, and returns that color.
      */
@@ -51,7 +50,7 @@ public class Color {
         int newBlue = this.blue + other.blue;
         return new Color(newRed, newGreen, newBlue);
     }
-    
+
     /**
      * Returns a darker version of this color (decreased by 32).
      */
@@ -61,20 +60,19 @@ public class Color {
         int newBlue = this.blue - 32;
         return new Color(newRed, newGreen, newBlue);
     }
-    
+
     /**
      * Determines whether two colors are equal.
      */
     public boolean equals(Object obj) {
         if (obj instanceof Color) {
             Color other = (Color) obj;
-            return this.red == other.red
-                && this.green == other.green
-                && this.blue == other.blue;
+            return this.red == other.red && this.green == other.green
+                    && this.blue == other.blue;
         }
         return false;
     }
-    
+
     /**
      * Returns a lighter version of this color (increased by 32).
      */
@@ -84,35 +82,43 @@ public class Color {
         int newBlue = this.blue + 32;
         return new Color(newRed, newGreen, newBlue);
     }
-    
-    
+
     /**
      * Subtracts the other color from this color, and returns that color.
      */
-    public Color sub(Color other) {
+    public Color subtract(Color other) {
         int newRed = this.red - other.red;
         int newGreen = this.green - other.green;
         int newBlue = this.blue - other.blue;
         return new Color(newRed, newGreen, newBlue);
     }
-    
+
     /**
-     * Returns the color in HTML format (RGB in hexidecimal).
+     * Returns the color in HTML format (RGB in hexadecimal).
      */
     public String toString() {
-        String redStr = Integer.toHexString(this.red);
-        if (redStr.length() == 1) {
-            redStr = "0" + redStr;
-        }
-        String greenStr = Integer.toHexString(this.green);
-        if (greenStr.length() == 1) {
-            greenStr = "0" + greenStr;
-        }
-        String blueStr = Integer.toHexString(this.blue);
-        if (blueStr.length() == 1) {
-            blueStr = "0" + blueStr;
-        }
-        return "#" + redStr + greenStr + blueStr;
+        return String.format("#%02x%02x%02x", this.red, this.green, this.blue);
     }
-    
+
+    /**
+     * Example use of Color objects.
+     */
+    public static void main(String[] args) {
+        Color black = new Color();
+        Color other = new Color(0, 0, 0);
+        Color gold = new Color(255, 215, 0);
+
+        System.out.println(black == other);
+        System.out.println(black.equals(other));
+        System.out.println();
+
+        System.out.println(black == gold);
+        System.out.println(black.equals(gold));
+        System.out.println();
+
+        System.out.println(black.toString());
+        System.out.println(gold.toString());
+        System.out.println();
+    }
+
 }
--- CS1/Act11/Point.java
+++ CS1/Act11/Point.java
@@ -1,20 +1,18 @@
 /**
- * A two-dimensional point.
+ * A two-dimensional point on the Cartesian plane.
  */
 public class Point {
-    
+
     private int x;
-    
     private int y;
-    
+
     /**
      * Constructs a point at the origin.
      */
     public Point() {
-        this.x = 0;
-        this.y = 0;
+        // attributes are automatically initialized to zero
     }
-    
+
     /**
      * Constructs a point at the given coordinates.
      */
@@ -22,61 +20,61 @@ public class Point {
         this.x = x;
         this.y = y;
     }
-    
+
     /**
      * Constructs a point at the same location.
      */
-    public Point(Point p) {
-        this.x = p.x;
-        this.y = p.y;
+    public Point(Point other) {
+        this.x = other.x;
+        this.y = other.y;
     }
-    
+
     /**
      * Determines whether two points are equal.
      */
     public boolean equals(Object obj) {
         if (obj instanceof Point) {
-            Point p = (Point) obj;
-            return this.x == p.x && this.y == p.y;
+            Point other = (Point) obj;
+            return this.x == other.x && this.y == other.y;
         }
         return false;
     }
-    
+
     /**
      * Gets the x value.
      */
-    public double getX() {
+    public int getX() {
         return this.x;
     }
-    
+
     /**
      * Gets the y value.
      */
-    public double getY() {
+    public int getY() {
         return this.y;
     }
-    
+
     /**
      * Sets the x value.
      */
     public void setX(int x) {
         this.x = x;
     }
-    
+
     /**
      * Sets the y value.
      */
     public void setY(int y) {
         this.y = y;
     }
-    
+
     /**
      * Returns a string representation of this point.
      */
     public String toString() {
         return String.format("(%d, %d)", this.x, this.y);
     }
-    
+
     /**
      * Example use of Point objects.
      */
@@ -84,15 +82,18 @@ public class Point {
         Point p1 = new Point();
         Point p2 = new Point(0, 0);
         Point p3 = new Point(3, 3);
-        
-        System.out.println( p1 == p1 );
-        System.out.println( p1.equals(p1) );
-        System.out.println( p1 == p2 );
-        System.out.println( p1.equals(p2) );
-        System.out.println( p1.equals(p3) );
-        System.out.println( p2.toString() );
-        System.out.println( p2.equals("(3, 3)") );
-        System.out.println( p3.equals("(3, 3)") );
+
+        System.out.println(p1 == p2);
+        System.out.println(p1.equals(p2));
+        System.out.println();
+
+        System.out.println(p1.toString());
+        System.out.println(p3.equals("(0, 0)"));
+        System.out.println();
+
+        System.out.println(p3.toString());
+        System.out.println(p3.equals("(3, 3)"));
+        System.out.println();
     }
-    
+
 }

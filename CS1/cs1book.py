bf4dd522d158ef0322395066271a31c39bdc8033
Chris Mayfield <mayfiecs@jmu.edu>
2021-07-19 19:59:34 -0400

    revise book for 2021

--- CS1/cs1book.py
+++ CS1/cs1book.py
@@ -4,7 +4,7 @@ import glob
 import sys
 
 def main(suff):
-    files = glob.glob("*/Act*.tex")
+    files = glob.glob("Act*/Act*.tex")
     for name in sorted(files):
         # get the title
         title = None
@@ -14,22 +14,31 @@ def main(suff):
                 break
         # output results
         if suff == "tex":
-            print "\\act{" + name[:-4] + "}{" + title + "}"
+            print "\\act{" + name[:-4] + "}{" + title + "}{"
         else:
             name = name[:name.find("/")]
             print "* [" + title + "](" + name + ")"
             continue
         # get model names (LaTeX only)
         mods = []
-        path = name[:name.rfind('/') + 1]
+        path = name[:name.rfind("/") + 1]
         for line in open(name):
             if line.startswith("\\input"):
                 if not line.endswith(".sty}\n"):
                     mods.append(path + line[7:-2])
+        # output models
+        i = 0
         for name in mods:
             for line in open(name):
                 if line.startswith("\\model"):
-                    print line[7:-2]
+                    i += 1
+                    print "\\mod{" + str(i) + "}",
+                    print line[7:-2], "\\\\"
+                if line.startswith("\\section*"):
+                    i += 1
+                    print "\\mod{" + str(i) + "}",
+                    print line[10:-2], "\\\\"
+        print "}"
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
--- CS1/pax.sh
+++ CS1/pax.sh
@@ -8,4 +8,9 @@
 #   pdfannotextractor --install
 
 for f in */Act*_Teacher.pdf; do pdfannotextractor $f; done
-for f in */Act*_Student.pdf; do pdfannotextractor $f; done
+for f in */Act*_Student.pdf; do pdfannotextractor $f 2>/dev/null; done
+
+# Redirecting student errors to /dev/null because of text boxes:
+#
+# !!! Warning: Annotation on page 1 not recognized!
+#     java.lang.Exception: Unsupported annotation subtype: Widget

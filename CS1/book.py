89f4a058bf02c45e25c219e8c6e9476045cc7adf
Chris Mayfield <mayfiecs@jmu.edu>
2021-07-19 21:21:52 -0400

    rename cs1book to book

--- CS1/book.py
+++ CS1/book.py
@@ -44,4 +44,4 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         main(sys.argv[1])
     else:
-        print("Usage: cs1book.py {tex | md}")
+        print("Usage: book.py {tex | md}")
